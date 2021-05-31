76cb20079626d8b2ce0414dfd737d8b890c4b5e1
surajp92 <pawarsuraj92@gmail.com>
2021-05-31 15:01:48 -0500

    comments added

--- airfoil_dnn_allmodels_tf_v2_m2.py
+++ airfoil_dnn_allmodels_tf_v2_m2.py
@@ -5,7 +5,7 @@ Created on Tue Nov  3 10:01:23 2020
 
 @author: suraj
 """
-
+# import all necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -29,18 +29,38 @@ from tensorflow.keras import regularizers
 
 #from helpers import get_lift_drag, preprocess_image, read_csv_file, save_model, load_model
 
+# font for plotting
 import matplotlib as mpl
 font = {'family' : 'normal',
         'size'   : 14}
 mpl.rc('font', **font)
 
 #%%
+
 def coeff_determination(y_true, y_pred):
+    '''
+    custom metric to evaluate the neural network training
+    it uses the coefficient of detrmination formula
+    for more information : https://en.wikipedia.org/wiki/Coefficient_of_determination
+    '''
     SS_res =  kb.sum(kb.square(y_true-y_pred ))
     SS_tot = kb.sum(kb.square( y_true - kb.mean(y_true) ) )
     return ( 1 - SS_res/(SS_tot + kb.epsilon()) )
 
 def training_data(data,panel_data,aoa,re,lift,drag):
+    '''
+    Input:
+    data ---- this contains the x and y coordinate of airfoil
+    panel_data ---- this contains the CL and CD_p determined using the panel method
+    aoa ---- angle of attack
+    re ---- Reynolds number
+    lift ---- lift coefficient from XFoil 
+    drag ---- drag coefficient from XFoil
+    
+    Output:
+    xtrain ---- input to the neural network for training (airfoil shape, aoa, re)
+    ytrain ---- label of the training dataset (Xfoil lift coefficient)    
+    '''
     num_samples = data.shape[0]
     npoints = data.shape[1]
     num_cp = npoints - 1
@@ -60,6 +80,20 @@ def training_data(data,panel_data,aoa,re,lift,drag):
     return xtrain, ytrain
 
 def training_data_cp(data,panel_data,aoa,re,lift,drag):
+    '''
+    Input:
+    data ---- this contains the x and y coordinate of airfoil
+    panel_data ---- this contains the CL and CD_p determined using the panel method
+    aoa ---- angle of attack
+    re ---- Reynolds number
+    lift ---- lift coefficient from XFoil 
+    drag ---- drag coefficient from XFoil
+    
+    Output:
+    xtrain ---- input to the neural network for training 
+                (airfoil shape, cp distribution from panel method, aoa, re)
+    ytrain ---- label of the training dataset (Xfoil lift coefficient)    
+    '''
     num_samples = data.shape[0]
     npoints = data.shape[1]
     num_cp = npoints - 1
@@ -80,6 +114,20 @@ def training_data_cp(data,panel_data,aoa,re,lift,drag):
     return xtrain, ytrain
 
 def training_data_cl_cd(data,panel_data,aoa,re,lift,drag):
+    '''
+    Input:
+    data ---- this contains the x and y coordinate of airfoil
+    panel_data ---- this contains the CL and CD_p determined using the panel method
+    aoa ---- angle of attack
+    re ---- Reynolds number
+    lift ---- lift coefficient from XFoil 
+    drag ---- drag coefficient from XFoil
+    
+    Output:
+    xtrain ---- input to the neural network for training 
+                (airfoil shape, CL and CD_p from panel method, aoa, re)
+    ytrain ---- label of the training dataset (Xfoil lift coefficient)    
+    '''
     num_samples = data.shape[0]
     npoints = data.shape[1]
     num_cp = npoints - 1
@@ -101,6 +149,20 @@ def training_data_cl_cd(data,panel_data,aoa,re,lift,drag):
     return xtrain, ytrain
 
 def training_data_cl(data,panel_data,aoa,re,lift,drag):
+    '''
+    Input:
+    data ---- this contains the x and y coordinate of airfoil
+    panel_data ---- this contains the CL and CD_p determined using the panel method
+    aoa ---- angle of attack
+    re ---- Reynolds number
+    lift ---- lift coefficient from XFoil 
+    drag ---- drag coefficient from XFoil
+    
+    Output:
+    xtrain ---- input to the neural network for training 
+                (airfoil shape, CL from panel method, aoa, re)
+    ytrain ---- label of the training dataset (Xfoil lift coefficient)    
+    '''
     num_samples = data.shape[0]
     npoints = data.shape[1]
     num_cp = npoints - 1
@@ -121,19 +183,27 @@ def training_data_cl(data,panel_data,aoa,re,lift,drag):
     return xtrain, ytrain
 
 #%%
-#input_shape = (64,64,1)
-#cd_scale_factor = 10
-#X1, X2, Y = read_csv_file(input_shape, data_version="v1", cd_scale_factor=10)
 
+# selct the model for training
+# POF paper uses im = 2
 im = 2 # 1: (x,y), 2: (x,y,cl,cd), 3: (x,y,Cp) 4: (x,y,cl)
 
-data_path = '../airfoil_shapes_re_v2/'
-#data_path = '../airfoil_shapes/'
+# define path/ folder which contains airfoil shape in csv format
+data_path = './airfoil_shapes_re_v2/'
 
+# number of cordinates defining the airfoil shape
 num_xy = 201
 num_cp = num_xy - 1
-  
+
+# train_data.csv file contains airfoil cordinates, aos, re, and CL, CD determined from XFoil 
+# the folder airfoil_shapes_re_v2 contains some python files that generates the data
+# airfoil_shape_generator.py : script for generating airfoil shape (x, y cordinates)
+# merge_files.py : for merginf all airfoil shapes in a single file
+# xfoil_analysis_v2.py: Ffor analyzing airfoils and generating CL, CD data using XFoil module in python
+
 db = data_path + 'train_data.csv'
+
+# read the data from train_data.csv file
 df = pd.read_csv(db, encoding='utf-8')
 
 col_name = []
@@ -154,9 +224,6 @@ df.columns = col_name
 
 df = df[df['CL'].notna()]
 
-#bad_airfoils = ['naca2206','naca4002','naca4404','naca4406','naca4602','naca6002','naca6004','naca6202','naca24002','naca24006','naca21004','naca22002','naca25002']
-#df = df[~(df["Airfoil"].isin(bad_airfoils))]
-
 num_samples = df.shape[0]
 
 panel_data = np.zeros((num_samples,num_cp+2))
@@ -176,6 +243,8 @@ airfoils_ycoords = df.iloc[:,202:403].values
 params = df.iloc[:,403:].values
 
 #%%
+# generate and save the required data for training 
+# (i.e, the variables required for creating xtrain and ytrain)
 counter = 0
 cd_scale_factor = 10.0
 
@@ -226,6 +295,7 @@ else:
     cm = data['cm']
     
 #%% 
+# generate xtrain and ytrain for training the neural network
 if im == 1:       
     xtrain, ytrain = training_data(data_xy,panel_data,aoa,re,cl,cd)
 elif im == 2:       
@@ -236,6 +306,7 @@ elif im == 4:
     xtrain, ytrain = training_data_cl(data_xy,panel_data,aoa,re,cl,cd)
     
 #%%
+# scale the input and output data between the range [-1, 1]
 sc_input = MinMaxScaler(feature_range=(-1,1))
 sc_input = sc_input.fit(xtrain)
 xtrain_sc = sc_input.transform(xtrain)
@@ -246,16 +317,20 @@ ytrain_sc = sc_output.transform(ytrain)
 
 xtrain, ytrain = xtrain_sc, ytrain_sc
 
+# only shape of the airfoil (x and y cordinates)
 xtrain1 = np.copy(xtrain[:,:2*num_xy])
+
+# aoa, re, and physics-based features from panel method
 xtrain2 = np.copy(xtrain[:,2*num_xy:])
 
 #%%
 training = True
-nf1 = xtrain1.shape[1]
-nf2 = xtrain2.shape[1]
-nl = ytrain.shape[1]
+nf1 = xtrain1.shape[1] # number of features related to airfoil shape
+nf2 = xtrain2.shape[1] # number of physics-based features related to flow
+nl = ytrain.shape[1] # number of labels (1 in POF paper, as we predict only lift coefficient)
 
 #%%
+# train 30 ensembles of neural network
 for i in range(1,31):
     seed_number = int(i*10)
     print(seed_number)
@@ -275,17 +350,12 @@ for i in range(1,31):
     
     x1 = Input(shape=(nf1,))
     x2 = Input(shape=(nf2,))
-    
-#    x = Dense(20,activation='relu',kernel_regularizer=regularizers.l2(1e-2))(x1)
-#    x = Dense(20,activation='relu',kernel_regularizer=regularizers.l2(1e-2))(x)
-    
+        
     x = Dense(20,activation='relu')(x1)
     x = Dense(20,activation='relu')(x)
     
+    # inject physics-based features at some intermediate hidden layes (3rd in this case)
     x = concatenate(inputs=[x, x2])
-
-#    x = Dense(20,activation='relu',kernel_regularizer=regularizers.l2(1e-2))(x)
-#    x = Dense(20,activation='relu',kernel_regularizer=regularizers.l2(1e-2))(x)
     
     x = Dense(20,activation='relu')(x)
     x = Dense(20,activation='relu')(x)
@@ -297,6 +367,7 @@ for i in range(1,31):
     print(model.summary())
     opt = tf.keras.optimizers.Adam(learning_rate=0.01)
     
+    """## compile the model"""
     model.compile(loss='mean_squared_error', optimizer=opt)
     
     """## Train the model"""
@@ -306,8 +377,13 @@ for i in range(1,31):
     tb = TensorBoard(log_dir='./logs', histogram_freq=10, batch_size=batch_size,
                     write_graph=True, write_images=True, write_grads=True)
     
-    history_callback = model.fit(x=[xtrain1, xtrain2], y=ytrain, batch_size=batch_size, epochs=epochs, \
-             validation_split=0.2, shuffle=True, callbacks=[tb])
+    history_callback = model.fit(x=[xtrain1, xtrain2], 
+                                 y=ytrain, 
+                                 batch_size=batch_size, 
+                                 epochs=epochs, 
+                                 validation_split=0.2, 
+                                 shuffle=True, 
+                                 callbacks=[tb])
     
     loss = history_callback.history["loss"]
     val_loss = history_callback.history["val_loss"]
@@ -327,12 +403,12 @@ for i in range(1,31):
     tf.reset_default_graph()
     
 #%%
+# predict the CL for NACA 23012 airfoil using all trained nerual networks (i.e., 30 ensembles)
 for i in range(1,31):
     seed_number = int(i*10)
     loaded_model = tf.keras.models.load_model(f'./model_{im}/ann_model_{im}_{seed_number}.h5')
         
     data_path = '../airfoil_shapes_re_v2/'   
-    #data_path = '../airfoil_shapes/'   
     
     db = data_path + 'test_data.csv'
     df = pd.read_csv(db, encoding='utf-8')
@@ -364,6 +440,7 @@ for i in range(1,31):
     
     generate_new_test_data = True
     
+    # import data needed for generating test data
     if generate_new_test_data :
         for index, row in df.iterrows():
             airfoil_names_test.append(row.Airfoil)
@@ -385,7 +462,6 @@ for i in range(1,31):
         np.savez('test_data_re_23012.npz',airfoil_names_test=airfoil_names_test, data_xy_test=data_xy_test,panel_data_test=panel_data_test,aoa_test=aoa_test,re_test=re_test,cl_test=cl_test,cd_test=cd_test,cm_test=cm_test)
     
     else:
-    #    data = np.load('test_data.npz')
         data = np.load('test_data_re_23012.npz')
         airfoil_names_test = data['airfoil_names_test']
         data_xy_test = data['data_xy_test']
@@ -396,6 +472,7 @@ for i in range(1,31):
         cd_test = data['cd_test']
         cm_test = data['cm_test']
     
+    # create test data for NACA 23012
     if im == 1:       
         xtest, ytest = training_data(data_xy_test,panel_data_test,aoa_test,re_test,cl_test,cd_test)
     elif im == 2:      
@@ -405,24 +482,23 @@ for i in range(1,31):
     elif im == 4:      
         xtest, ytest = training_data_cl(data_xy_test,panel_data_test,aoa_test,re_test,cl_test,cd_test)
     
+    # scale the test data
     xtest_sc = sc_input.transform(xtest[:,:])  
-    xtest_sc1 = np.copy(xtest_sc[:,:2*num_xy])
-    xtest_sc2 = np.copy(xtest_sc[:,2*num_xy:])
+    xtest_sc1 = np.copy(xtest_sc[:,:2*num_xy]) # airfol shape features
+    xtest_sc2 = np.copy(xtest_sc[:,2*num_xy:]) # physics-based features features
     
+    # predict the lift coefficient for NACA 23012 airfoil
     ypred_sc = loaded_model.predict([xtest_sc1,xtest_sc2]) 
-    ypred_sc1 = loaded_model.predict([xtest_sc1,xtest_sc2]) 
     ypred = sc_output.inverse_transform(ypred_sc)
     
     fig,ax = plt.subplots(1,2, figsize=(12,5))
     
     ax[0].plot(aoa_test[:num_samples], ytest[:num_samples,0], 'ro-', label='Xfoil NACA23012')
     ax[0].plot(aoa_test[:num_samples], ypred[:num_samples,0], 'bo-', label=f'ML-{im} NACA23012')
-    #ax[0].plot(a, cl, 'bo-', label='Xfoil NACA0012')
     ax[0].legend()
     
     ax[1].plot(aoa_test[:num_samples], ytest[:num_samples,0], 'ro-', label='Xfoil NACA0012 ')
     ax[1].plot(aoa_test[:num_samples], ypred[:num_samples,0], 'bo-', label=f'ML-{im} NACA0012 ')
-    #ax[1].plot(a, cd, 'bo-', label='Xfoil NACA0012 ')
     ax[1].legend()
     
     ax[0].set_ylabel(r'$C_L$')
@@ -449,12 +525,12 @@ for i in range(1,31):
     file.close()
 
 #%%
+# predict the CL for NACA 23024 airfoil using all trained nerual networks (i.e., 30 ensembles)
 for i in range(1,31):
     seed_number = int(i*10)
     loaded_model = tf.keras.models.load_model(f'./model_{im}/ann_model_{im}_{seed_number}.h5')
         
     data_path = '../airfoil_shapes_re_v2/'   
-    #data_path = '../airfoil_shapes/'   
     
     db = data_path + 'test_data.csv'
     df = pd.read_csv(db, encoding='utf-8')
@@ -486,6 +562,7 @@ for i in range(1,31):
     
     generate_new_test_data = True
     
+    # import data needed for generating test data
     if generate_new_test_data :
         for index, row in df.iterrows():
             airfoil_names_test.append(row.Airfoil)
@@ -507,7 +584,6 @@ for i in range(1,31):
         np.savez('test_data_re_23024.npz',airfoil_names_test=airfoil_names_test, data_xy_test=data_xy_test,panel_data_test=panel_data_test,aoa_test=aoa_test,re_test=re_test,cl_test=cl_test,cd_test=cd_test,cm_test=cm_test)
     
     else:
-    #    data = np.load('test_data.npz')
         data = np.load('test_data_re_23024.npz')
         airfoil_names_test = data['airfoil_names_test']
         data_xy_test = data['data_xy_test']
@@ -518,6 +594,7 @@ for i in range(1,31):
         cd_test = data['cd_test']
         cm_test = data['cm_test']
     
+    # create test data for NACA 23012
     if im == 1:       
         xtest, ytest = training_data(data_xy_test,panel_data_test,aoa_test,re_test,cl_test,cd_test)
     elif im == 2:      
@@ -527,24 +604,23 @@ for i in range(1,31):
     elif im == 4:      
         xtest, ytest = training_data_cl(data_xy_test,panel_data_test,aoa_test,re_test,cl_test,cd_test)
     
+    # scale the test data
     xtest_sc = sc_input.transform(xtest[:,:])  
-    xtest_sc1 = np.copy(xtest_sc[:,:2*num_xy])
-    xtest_sc2 = np.copy(xtest_sc[:,2*num_xy:])
+    xtest_sc1 = np.copy(xtest_sc[:,:2*num_xy]) # airfol shape features
+    xtest_sc2 = np.copy(xtest_sc[:,2*num_xy:]) # physics-based features features
     
+    # predict the lift coefficient for NACA 23024 airfoil
     ypred_sc = loaded_model.predict([xtest_sc1,xtest_sc2]) 
-    ypred_sc1 = loaded_model.predict([xtest_sc1,xtest_sc2]) 
     ypred = sc_output.inverse_transform(ypred_sc)
     
     fig,ax = plt.subplots(1,2, figsize=(12,5))
     
     ax[0].plot(aoa_test[:num_samples], ytest[:num_samples,0], 'ro-', label='Xfoil NACA23024')
     ax[0].plot(aoa_test[:num_samples], ypred[:num_samples,0], 'bo-', label=f'ML-{im} NACA23024')
-    #ax[0].plot(a, cl, 'bo-', label='Xfoil NACA0012')
     ax[0].legend()
     
     ax[1].plot(aoa_test[:num_samples], ytest[:num_samples,0], 'ro-', label='Xfoil NACA0024')
     ax[1].plot(aoa_test[:num_samples], ypred[:num_samples,0], 'bo-', label=f'ML-{im} NACA0024')
-    #ax[1].plot(a, cd, 'bo-', label='Xfoil NACA0012 ')
     ax[1].legend()
     
     ax[0].set_ylabel(r'$C_L$')
